5a75c97985f6c8feff68af0e2533e0563abea158
Bhugol123 <bhugolkc07@gmail.com>
2024-08-16 15:21:46 -0700

    ch

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,15 +37,15 @@ const handleExtraSpace =()=>{
         <h2>{heading} below</h2>
         <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor: mode==='dark'? 'gray': 'white', color: mode==='dark'? 'white': '#042743' }} value={text} id="myBox" rows="8"></textarea>
          </div>
-          <button className='btn btn-primary mx-3 ' onClick={handleUpClick}>Convert to uppercase</button>
-          <button className='btn btn-primary mx-3' onClick={handleLowerClick}>Convert to lowercase</button>
-          <button className='btn btn-primary mx-3' onClick={handleClearClick}>Clear text</button>
-          <button className='btn btn-primary mx-3' onClick={handleExtraSpace}>Clear space</button>
+          <button className='btn btn-primary mx-3 my-2 ' onClick={handleUpClick}>Convert to uppercase</button>
+          <button className='btn btn-primary mx-3 my-2' onClick={handleLowerClick}>Convert to lowercase</button>
+          <button className='btn btn-primary mx-3 my-2' onClick={handleClearClick}>Clear text</button>
+          <button className='btn btn-primary mx-3 my-2' onClick={handleExtraSpace}>Clear space</button>
     </div>
 
     <div className="container my-3">
         <h3>Text you have enter.</h3>
-        <p>{text.split(' ').length - 1} words, {text.length} charcters</p>
+        <p>{text.split(' ').filter((element)=>{return element.length!==0}).length } words, {text.length} charcters</p>
         <p>{0.008 * text.split(' ').length} minutes is required to read {text.split(' ').length } words.</p>
         <h3>Preview</h3>
         <p>{text.length>0?text:'Enter somethings in the box to preview'}</p>
